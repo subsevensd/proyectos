6e40ce037f1d4686515567d876390d6d52e8b904
Silampaf <silampaf@hotmail.com>
2013-04-02 02:40:09 -0600

    Empeze el archivo eng.php

---
+++ cookies/eng.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hi this is english page</title>
+<meta charset="utf-8"/>
+<style type="text/css">
+body{
+	background-color:green;
+	color:white;
+
+}
+
+</style>
+
+
+</head>
+<body>
+	<header>
+		<hgroup>
+			<h1>hi welcome to the biggest game page</h1>
+			<h2> Below is an interesting range of games you can download</h2>     
+		
+		
+		</hgroup>
+		
+	
+	</header>
+	<section>
+		<article>
+			<br><br><br>
+			<p>Play Batman</p>
+			<p>Play rock,paper or scissor</p>
+		
+		</article>
+	
+	
+	</section>
+
+
+
+</body>
+
+
+
+</html>
