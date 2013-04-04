ab0e19a7d90b7fbc836146243b661b860b010db6
mugre <mugre@gmail.com>
2013-04-03 21:17:44 -0600

    Version funcional

---
+++ cookies/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Idioma</title>
+  <style type = "text/css">
+    body
+      {
+        background-color:black;
+        color:white;
+      }
+      a{
+        list-style:none;
+        color:green;
+      }
+	  a:visited{
+		color:red;
+	  
+	  
+	  }
+        
+    
+    
+    
+  </style>
+  
+  <meta charset="utf-8">
+  
+  
+  
+</head>
+<body>
+  <header>
+    <hgroup>
+      <h1>Bienvenido al portal de juegos mas grande</h1>
+      <h2>Selecciona el lenguaje</h2>
+      
+    </hgroup>
+    
+    
+  </header>
+  <section>
+    <article>
+      <a href ="grabacookie.php?idioma=en">English Version</a> <br>
+      <a href ="grabacookie.php?idioma=es">Versión en español</a>
+	  
+	  
+
+      
+      
+    </article>
+    
+    
+    
+  </section>
+  
+  
+  
+</body>
+  
+  
+  
+  
+  
+  
+</html>
